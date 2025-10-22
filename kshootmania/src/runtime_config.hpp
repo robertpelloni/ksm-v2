d3edea7ab5c0b86591903f839d1d276fb762e045
masaka <masaka@mz-kb.com>
2025-10-22 13:53:19 +0900

    RuntimeConfigのコミット漏れ修正

---
+++ kshootmania/src/runtime_config.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "common/common_defines.hpp"
+
+// 実行中のみ保持する設定
+// config.iniには保存されず、起動時にデフォルト値にリセットされる
+namespace RuntimeConfig
+{
+	GaugeType GetGaugeType();
+
+	void SetGaugeType(GaugeType gaugeType);
+
+	TurnMode GetTurnMode();
+
+	void SetTurnMode(TurnMode turnMode);
+
+	JudgmentPlayMode GetJudgmentPlayModeBT();
+
+	void SetJudgmentPlayModeBT(JudgmentPlayMode mode);
+
+	JudgmentPlayMode GetJudgmentPlayModeFX();
+
+	void SetJudgmentPlayModeFX(JudgmentPlayMode mode);
+
+	JudgmentPlayMode GetJudgmentPlayModeLaser();
+
+	void SetJudgmentPlayModeLaser(JudgmentPlayMode mode);
+
+	// config.iniから判定モードを復元
+	void RestoreJudgmentModesFromConfigIni();
+}
