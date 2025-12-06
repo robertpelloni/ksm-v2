75a4a1a4d71a2c0a405fdd9456a83576ef2fc46f
masaka <masaka@mz-kb.com>
2025-12-06 13:28:19 +0900

    NocoUI更新に伴う関数名変更、VerticalMarqueeがnoco名前空間に入ってしまっていたのを修正

--- kshootmania/src/NocoExtensions/VerticalMarquee.hpp
+++ kshootmania/src/NocoExtensions/VerticalMarquee.hpp
@@ -1,36 +1,33 @@
-#pragma once
+﻿#pragma once
 
-namespace noco
+class VerticalMarquee : public noco::ComponentBase
 {
-	class VerticalMarquee : public ComponentBase
+private:
+	double m_startWaitDurationSec;
+	double m_endWaitDurationSec;
+	double m_speed;
+
+	enum class State
 	{
-	private:
-		double m_startWaitDurationSec;
-		double m_endWaitDurationSec;
-		double m_speed;
-
-		enum class State
-		{
-			StartWait,
-			Scrolling,
-			EndWait,
-		};
-
-		State m_state;
-		Stopwatch m_stopwatch;
-
-	public:
-		explicit VerticalMarquee(Duration startWaitDuration = 1s, Duration endWaitDuration = 1s, double speed = 50.0)
-			: ComponentBase({})
-			, m_startWaitDurationSec(startWaitDuration.count())
-			, m_endWaitDurationSec(endWaitDuration.count())
-			, m_speed(speed)
-			, m_state(State::StartWait)
-		{
-		}
-
-		void onActivated(const std::shared_ptr<Node>& node) override;
-
-		void update(const std::shared_ptr<Node>& node) override;
+		StartWait,
+		Scrolling,
+		EndWait,
 	};
-}
+
+	State m_state;
+	Stopwatch m_stopwatch;
+
+public:
+	explicit VerticalMarquee(Duration startWaitDuration = 1s, Duration endWaitDuration = 1s, double speed = 50.0)
+		: ComponentBase({})
+		, m_startWaitDurationSec(startWaitDuration.count())
+		, m_endWaitDurationSec(endWaitDuration.count())
+		, m_speed(speed)
+		, m_state(State::StartWait)
+	{
+	}
+
+	void onActivated(const std::shared_ptr<noco::Node>& node) override;
+
+	void update(const std::shared_ptr<noco::Node>& node) override;
+};
